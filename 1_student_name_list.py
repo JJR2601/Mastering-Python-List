6933a600da71d6505a76d719ef789dc06f72d9d4
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 16:03:21 +0800

    Names of students List Manipulation

---
+++ 1_student_name_list.py
@@ -0,0 +1,20 @@
+# A. Create a list of 20 student names
+student_name = ["justine", "jerald", "julia", "jasmine", "july", "june", "julie", "jade", "jude", "jack", "james",
+                "jane", "juniper", "jasper", "jonah", "jennifer", "jordan", "julian", "jacob", "jet"]
+
+# B. Access and print the 15th index
+print(student_name[15])
+
+# C. Update the 12th index to "John"
+student_name[12] = "john"
+print(student_name)
+
+# D. Delete the 10th index
+del student_name[10]
+print(student_name)
+
+# E. Slice the list from index 2 to 5 and print the sliced portion
+print(student_name[2:5])
+
+# F. Print the last index of the list
+print(student_name[-1])
