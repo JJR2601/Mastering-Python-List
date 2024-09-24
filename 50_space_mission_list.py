6793a6e93b3a3cc94c9ae0cc9ce1f80e29eb2bae
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 23:05:09 +0800

    Accessing the list of space missionss

---
+++ 50_space_mission_list.py
@@ -0,0 +1,12 @@
+space_missions = ["the interstellar mission", "aditya-l1", "2001 mars odyssey", "danvici", "hera", "europa clipper",
+                  "dragonfly", "bepicolombo", "comet interceptor", "emirates mars mission"]
+
+print(space_missions)
+
+print(space_missions[-3])
+
+space_missions[4] = "apollo 11"
+
+del space_missions[6]
+
+print(space_missions[-1])
