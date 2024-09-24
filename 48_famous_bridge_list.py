43bb87aba6460d665f660dfcdabcd34cfe980c3e
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:56:55 +0800

    List of Famous Bridges in the world Manipulation

---
+++ 48_famous_bridge_list.py
@@ -0,0 +1,11 @@
+bridges = ["golden gate", "ponte veccio", "sydney harbour", "london", "hangzhou bay", "mackinac", "tower", "westminster"]
+
+print(bridges)
+
+print(bridges[3])
+
+bridges[6] = "golden gate"
+
+del bridges[-3]
+
+print(bridges[-1])
