5dd13226bee7b33f4f7e7b7e8a99d6a1bc846565
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 15:02:39 +0800

    Student name list

---
+++ student_names.py
@@ -0,0 +1,4 @@
+student_name_list = {"justine", "jerald", "jose", "julia", "jorge", "justin", "lilith", "ivy", "joshua", "jasmine",
+                    "aga", "jackson", "liz", "charlie", "noah", "tyler", "hiro", "olivia", "queen", "zosia"}
+
+print(student_name_list)
