2bc5e39e919ba392164885b5825f57da499c109d
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:40:09 +0800

    List of famous actors and manipulating it

---
+++ 28_actor_list.py
@@ -0,0 +1,12 @@
+actors = ["kevin hart" , "vin diesel", "arnold schwarzenegger", "jackie chan", "tom holland", "chris pratt", "brad pitt"
+          , "rowan atkinson", "chris evans", "paul walker"]
+
+print(actors)
+
+print(actors[-3])
+
+actors[3] = "leonardo dicaprio"
+
+del actors[-2]
+
+print(actors[-1])
