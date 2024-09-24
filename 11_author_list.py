3691713076a9e9960f5a4cc6ef475e9d40d691ce
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 18:07:00 +0800

    Famous author around the world list Manipulation

---
+++ 11_author_list.py
@@ -0,0 +1,21 @@
+# A. Create a list of 12 famous authors
+author_list = ["eiichiro oda", "akira toriyama", "shogo kinugasa", "william shakespeare", "lewis carroll", "robert greene"
+               , "cervantes", "gustave flaubert", "stan lee", "anne frank", "homer", "victor hugo"]
+
+# B. Print the entire list
+print(author_list)
+
+# C. Access and print the 6th index
+print(author_list[6])
+
+# D. Update the 4th index to "Mark Twain"
+author_list[4] = "mark twain"
+
+# E. Delete the 10th index
+del author_list[-2]
+
+# F. Slice the list from index 3 to 8
+author_list[3:8]
+
+# g. Print the last index
+print(author_list[-1])
