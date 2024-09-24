8a2d53a77e636117ad7a28cc02723bc305c23a23
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:23:21 +0800

    manipulating the list of maybe the best genres earlier

---
+++ 26_music_genre_list.py
@@ -0,0 +1,14 @@
+music_genre = ["rap", "rock", "country", "folk", "blues", "jazz", "metal", "pop" , "indie", "classical", "symphony", "chant"
+               , "choral", "opera", "a cappella"]
+
+print(music_genre)
+
+print(music_genre[-5])
+
+music_genre[5] = "jazz"
+
+del music_genre[7]
+
+music_genre[3:8]
+
+print(music_genre[-1])
