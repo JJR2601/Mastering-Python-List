8a7f26b13a931c2c454069cf463e9ae25b22710a
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 18:50:05 +0800

    List of most famous companies in the world

---
+++ 14_company_list.py
@@ -0,0 +1,17 @@
+# A. Create a list of 10 company names.
+companies = ["toyota", "apple", "amazon", "nestle", "google", "walmart", "shell", "microsoft", "coca cola", "mitsubishi"]
+
+# B. Print the entire list.
+print(companies)
+
+# C. Access and print the 7th index.
+print(companies[7])
+
+# D. Update the 5th index to "Apple".
+companies[5] = "apple"
+
+# E. Delete the 8th index.
+del companies[-2]
+
+# F. Print the last index
+print(companies[-1])
