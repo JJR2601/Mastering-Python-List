ae3d1f8bdf4160c04b11ad508bcdcf5a11c9a694
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:22:56 +0800

    Accessing the list of prehistoric animals

---
+++ 42_dinosaur_list.py
@@ -0,0 +1,12 @@
+dinosaur = ["tyrannosaurus", "triceratops", "brachiosaurus", "stegosaurus", "velociraptor", "ankylosaurus", "spinosaurus"
+            , "allosaurus"]
+
+print(dinosaur)
+
+print(dinosaur[4])
+
+dinosaur[-2] = "tyrannosaurus rex"
+
+del dinosaur[-1]
+
+print(dinosaur[-1])
