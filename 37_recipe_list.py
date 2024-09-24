e03bd1b57a7fddd1c7330baaee585971252bc382
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 21:45:48 +0800

    Accessing list of filipino recipes

---
+++ 37_recipe_list.py
@@ -0,0 +1,14 @@
+recipes = ["pork adobo", "tokwa't baboy", "pork menudo", "pork sinigang", "lechon kawali", "tinolang maok", "sopas", "paksiw",
+           "adobong pusit", "beef mechado", "bistek", "beef tapa", "chop seuy", "fried rice", "pinakbet"]
+
+print(recipes)
+
+print(recipes[-3])
+
+recipes[9] = "lasagna"
+
+del recipes[-4]
+
+recipes[5:-5]
+
+print(recipes[-1])
