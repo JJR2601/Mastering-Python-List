3fe50531b0c1ae5a672c86cbf6a9a20e832935e8
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:00:08 +0800

    list of all continents

---
+++ 22_continent_list.py
@@ -0,0 +1,11 @@
+continent = ["europe", "australia", "asia", "africa", "north americal", "south america", "antartica"]
+
+print(continent)
+
+print(continent[4])
+
+continent[2] = "africa"
+
+del continent[-1]
+
+print(continent[-1])
