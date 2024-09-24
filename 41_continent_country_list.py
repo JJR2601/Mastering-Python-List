250888e55e89c2b393285f92598a61d13e1d8e9f
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:18:46 +0800

    Accessing list of countries and its continent

---
+++ 41_continent_country_list.py
@@ -0,0 +1,15 @@
+continent_country = ["zimbabwe: africa ", "madagascar: africa ", "mozambique: africa", "philippines: asia", "indonesia: asia"
+                     , "japan: asia", "andorra: europe", "denmark, europe", "finland: europe", "canada: north americal",
+                     " costa rica: north america", "haiti: north america", "new zealand: australia", "papua new guinea: australia", "solomon islands: australia"]
+
+print(continent_country)
+
+print(continent_country[9])
+
+continent_country[-5] = "australia"
+
+del continent_country[-3]
+
+continent_country[4:8]
+
+print(continent_country[-1])
