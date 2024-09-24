5690ac63e50025f5f814bc73c1f5c49709023b71
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:51:33 +0800

    Accessing the list of the most devastating volcanoes ever

---
+++ 47_waterfall_list.py
@@ -0,0 +1,11 @@
+waterfall = ["niagara", "iguazu", "gullfoss", "rhine", "angel", "jog", "kaieteur", "yosemite", "sutherland", "victoria"]
+
+print(waterfall)
+
+print(waterfall[-2])
+
+waterfall[5] = "niagra"
+
+del waterfall[-1]
+
+print(waterfall[-1])
