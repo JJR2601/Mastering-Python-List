a0bd50e20e7748c7ea3f8383a60b9fb553587495
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 19:46:37 +0800

    Accessing the most famous desserts in the world

---
+++ 20_dessert_list.py
@@ -0,0 +1,17 @@
+# A. Create a list of 10 desserts
+desserts = ["parfait", "dondurma", "ice cream" ,"tembleque", "tespishte", "pie", "cake", "leche plan", "crepes", "donut"]
+
+# B. Print the entire list
+print(desserts)
+
+# C. Accessing and print the 4th index
+print(desserts[4])
+
+# D. Update the 6th index to "Chocolate Cake"
+desserts[6] = "chocolate cake"
+
+# E. Delete the 3rd index
+del desserts[3]
+
+# F. Print the last index
+print(desserts[-1])
