d2e5247004d67e81e6426655a23b08929827e961
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:06:28 +0800

    Manipulating the list of comfy furnitures

---
+++ 23_furniture_list.py
@@ -0,0 +1,12 @@
+furnitures = ["cabinet", "bed", "chair", "sofa", "table", "drawer", "desk", "tv stand", "bookcases",
+              "bench", "rocking chair", "stool"]
+
+print(furnitures)
+
+print(furnitures[-4])
+
+furnitures[5] = "sofa"
+
+del furnitures[-2]
+
+print(furnitures[-1])
