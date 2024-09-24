85dd1a42bba1e8b503d5cc0185e7b5bd8084c70b
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 18:31:43 +0800

    List of Tall mountains Manipulation

---
+++ 13_mountain_list.py
@@ -0,0 +1,17 @@
+# A. Create a list of 8 mountain names
+mountains = ["apo", "fuji", "kanlaon", "lhotse", "makalu", "dhaulagiri", "annapurna", "k2"]
+
+# B. Print the entire list
+print(mountains)
+
+# C. Access and print the 5th index
+print(mountains[5])
+
+# D. Update the 3rd index to "Everest"
+mountains[3] = "everest"
+
+# E. Delete the 6th index
+del mountains[-2]
+
+# F. Print the last index
+print(mountains[-1])
