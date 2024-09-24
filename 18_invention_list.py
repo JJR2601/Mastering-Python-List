f4f1d5e416416206f4c5274faffe9c3aa0c7364f
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 19:30:10 +0800

    Manipulating the list of risky inventions

---
+++ 18_invention_list.py
@@ -0,0 +1,17 @@
+# A. Create a list of 8 famous inventions
+inventions = ["air balloon", "computer", "atomic bomb", "automobile", "barbed wire", "bicycle", "bullet proof vests", "camera"]
+
+# B. Print the entire list
+print(inventions)
+
+# C. Access and print the 6th index
+print(inventions[-2])
+
+# D. Update the 2nd index to "Lightbulb"
+inventions[2] = "lightbulb"
+
+# E. Delete the 5th index
+del inventions[-3]
+
+# F. Print the last index
+print(inventions[-1])
