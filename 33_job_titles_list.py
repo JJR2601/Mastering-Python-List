414478abfd5428d0f282e453945631d07d6ce0b5
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 21:22:49 +0800

    Assessing the List of Job titles

---
+++ 33_job_titles_list.py
@@ -0,0 +1,12 @@
+job_titles = ["medical assistant", "project manager", "account executive", "it professional", "software engineer", "web developer"
+              , "ux designer", "ui developer", "computer scientist", "application developer"]
+
+print(job_titles)
+
+print(job_titles[6])
+
+job_titles[5] = "software engineer"
+
+del job_titles[9]
+
+print(job_titles[-1])
