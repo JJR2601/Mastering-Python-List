0868bf010fd0d38ff4596390efab1696487eabfe
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:41:56 +0800

    Manipulating the list of random rivers in the world

---
+++ 45_river_list.py
@@ -0,0 +1,12 @@
+rivers = ["baghmati river", "aras river", "diyala river", "qishon river", "orontes river", "aldan river", "daly river"
+          , " inn river", "torne river", "tisza river"]
+
+print(rivers)
+
+print(rivers[6])
+
+rivers[-2] = "nile river"
+
+del rivers[-1]
+
+print(rivers[-1])
