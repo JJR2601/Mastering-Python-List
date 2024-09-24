64eca4c6cce4b8c0605072384d232a9c9a202704
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 17:29:40 +0800

    Manipulating the planets of solar system

---
+++ 8_planet_list.py
@@ -0,0 +1,17 @@
+# A. Create a list of 8 planet names
+planet_list = ["mars", "earth", "jupiter", "venus", "saturn", "mercury", "neptune", "uranus"]
+
+# B. Print the entire list
+print(planet_list)
+
+# C. Access and print the 3rd index
+print(planet_list[3])
+
+# D. Update the 7th index to "Pluto"
+planet_list[7] = "pluto"
+
+# E. Delete the 4th index
+del planet_list[4]
+
+# F. Print the last index
+print(planet_list[-1])
