13cee6dee9313742be22a2db57ef34ff867fe258
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 21:32:15 +0800

    Accessing the list of bird species

---
+++ 35_bird_species_list.py
@@ -0,0 +1,14 @@
+bird_species = ["ostrich", "kiwi", "moa", "turkey", "emu", "quail", "rhea", "chicken", "cassowary", "elephant bird",
+                "pheasant", "tinamou"]
+
+print(bird_species)
+
+print(bird_species[-3])
+
+bird_species[6] = "eagle"
+
+del bird_species[8]
+
+bird_species[3:7]
+
+print(bird_species[-1])
