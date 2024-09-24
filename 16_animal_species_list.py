2ecbefe6178f97437c009486b373444fce83b428
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 19:15:40 +0800

    Assessing the list of might be endangered animal species

---
+++ 16_animal_species_list.py
@@ -0,0 +1,17 @@
+# A. Create a list of 7 animal species
+animal_species = ["saola", "vaquita", "blue whale", "bonobo", "sea lions", "whale shark", "dugong"]
+
+# B. Print the entire list
+print(animal_species)
+
+# C. Access and print the 2nd index
+print(animal_species[2])
+
+# D. Update the 5th index to "Gorilla"
+animal_species[-2] = "gorilla"
+
+# E. Delete the 6th index
+del animal_species[-1]
+
+# F. Print the last index
+print(animal_species[-1])
