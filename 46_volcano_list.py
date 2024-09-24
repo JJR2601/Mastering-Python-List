f710436a6d922d44b4afa9cb8a77e3404352e8d5
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:47:43 +0800

    Accessing the list of the most devastating volcanoes ever

---
+++ 46_volcano_list.py
@@ -0,0 +1,11 @@
+volcanoes = ["krakatoa", "mt pinatubo", "mt mayon", "mt fuji", "mt tambora", "laki", "santa maria"]
+
+print(volcanoes)
+
+print(volcanoes[4])
+
+volcanoes[3] = "mt vesuvius"
+
+del volcanoes[-2]
+
+print(volcanoes)
