3f5ee06daeed911ef066108f9a31adbbdeb63a03
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:01:05 +0800

    Assessing the list of festival celebration in the philippines

---
+++ 40_festival_celebration_list.py
@@ -0,0 +1,12 @@
+festival_celebration = ["sinulog", "ati-atihan", "dinagyang", "higantes", "masskara", "kadayawan", "pahiyas", "lanzones"
+                         , "panagbenga", "giant lantern"]
+
+print(festival_celebration)
+
+print(festival_celebration[5])
+
+festival_celebration[3] = "thanksgiving"
+
+del festival_celebration[-3]
+
+print(festival_celebration[-1])
