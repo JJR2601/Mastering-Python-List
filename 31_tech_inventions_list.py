7c931d9a6211c473c7e1b55f238867ca464ecea1
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 21:13:48 +0800

    Best inventions ever created list manipulation

---
+++ 31_tech_inventions_list.py
@@ -0,0 +1,14 @@
+tech_inventions = ["computer", "telephone", "smartphone", "3d printing", "ai", "social media", "lightbulb", "camera", "xray", "radar"
+                    "email", "internet", "gps", "world wide web", "wifi"]
+
+print(tech_inventions)
+
+print(tech_inventions[-4])
+
+tech_inventions[8] = "smartphone"
+
+del tech_inventions[-1]
+
+tech_inventions[5:10]
+
+print(tech_inventions[-1])
