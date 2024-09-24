3efafb3b12b4f227ad094c0c0dd6158f5783314f
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:16:56 +0800

    Manipulating the list of most popular sports

---
+++ 25_sports_list.py
@@ -0,0 +1,11 @@
+sports = ["soccer", "basketball", "baseball", "volleyball", "badminton", "chess", "boxing", "hockey", "tennis", "golf"]
+
+print(sports)
+
+print(sports[6])
+
+sports[4] = "basketball"
+
+del sports[-1]
+
+print(sports[-1])
