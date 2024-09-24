1dc32ffcba5f5ba249b2a274eb2c3931a3c2935c
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 21:50:21 +0800

    8 of the most popular drink right now list

---
+++ 38_drink_list.py
@@ -0,0 +1,11 @@
+drinks = ["water", "coca cola", "sprite", "beer", "pepsi", "fanta", "7up", "coffee"]
+
+print(drinks)
+
+print(drinks[4])
+
+drinks[3] = "coffee"
+
+del drinks[-1]
+
+print(drinks[-1])
