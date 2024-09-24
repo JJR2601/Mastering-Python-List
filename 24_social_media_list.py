c8df509b79f3c2c380a1f1d10963003649f2279f
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:11:00 +0800

    Most used social media list manipulation

---
+++ 24_social_media_list.py
@@ -0,0 +1,11 @@
+socmed = ["facebook", "reddit", "youtube", "instagram". "tumblr", "x", "whatsapp", "telegram", "tiktok", "medium"]
+
+print(socmed)
+
+print(socmed[5])
+
+socmed[3] = "instagram"
+
+del socmed[-2]
+
+print(socmed[-1])
