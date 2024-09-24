8bbf33aff9ad6660fea2fd9c2c05f888c93dfb4c
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 17:45:09 +0800

    Popular Programming language list Manipulation

---
+++ 9_language_list.py
@@ -0,0 +1,21 @@
+# A. Create a list of 14 programming languages
+prog_language = ["python", "c#", "c++", "java script", "java", "html", "php", "sql", "c", "go", "css", "swift", "ruby",
+                 "rust"]
+
+# B. Print the entire list
+print(prog_language)
+
+# C. Access and print the 11th index
+print(prog_language[-3])
+
+# D. Update the 9th index to "Python"
+prog_language[9] = "python"
+
+# E. Delete the 13th index
+del prog_language[-2]
+
+# F. Slice the list from index 5 to 12
+prog_language[5:-2]
+
+# G. Print the last index
+print(prog_language[-1])
