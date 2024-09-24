3119c0310f3621601280f7d5601fd630f561b7f0
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:35:37 +0800

    Manipulating the list of fastest land animals in the world

---
+++ 44_land_animal_speed_list.py
@@ -0,0 +1,15 @@
+land_animal_speed = ["cheetah 65mph", "pronghorn 62mph", "dorcas gazelle 50mph", "the quarter horse 44mph", "ostrich 43.5",
+                     "greyhound 37.9mph" ,"moose 37.3mph", "zebra 35.8mph" , "wildebeest 32mph", "lion 31.19mph",
+                     "kangaroo 44mph", "african wild dog 44mph"]
+
+print(land_animal_speed)
+
+print(land_animal_speed[7])
+
+land_animal_speed[9] = "cheetah 120kmh"
+
+del land_animal_speed[-2]
+
+land_animal_speed[3:7]
+
+print(land_animal_speed[-1])
