a9238d1bbd4f95eeb4df7d56dad7f6e4e98b80e8
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:48:51 +0800

    List of school subjects

---
+++ 29_school_subject_list.py
@@ -0,0 +1,14 @@
+school_subject = ["english", "math", "science", "chemistry", "filipino", "arts", "music", "p.e.", "arts", "esp", "araling panlipunan",
+                  "tle"]
+
+print(school_subject)
+
+print(school_subject[6])
+
+school_subject[8] = "mathematics"
+
+del school_subject[4]
+
+school_subject[2:5]
+
+print(school_subject[-1])
