20ce3813255eed4f3a5e94e53a60505ddc50d5ba
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:59:09 +0800

    List of maybe the best festivals in the world

---
+++ 21_festival_list.py
@@ -0,0 +1,14 @@
+festival = ["sinulog", "burning man", "carnival", "la tomatina", "mardi gras" ,"dia de los muertos", "krampusnacht", "songkran"
+            , "st. patricks festival", "holi festival", "vesak", "gion matsuri", "montreux jazz", "mevlana", "nozawa onsen fires"]
+
+print(festival)
+
+print(festival[7])
+
+festival[-4] = "diwali"
+
+del festival[9]
+
+festival[5:11]
+
+print(festival[-1])
