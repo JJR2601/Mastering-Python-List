08394deb7d26f6093972bb46b4f1106c2b93b1c1
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 16:11:34 +0800

    Manipulating the list of Colors

---
+++ 3_color_list.py
@@ -0,0 +1,21 @@
+# A. Create a list of 15 colors
+color_list = ["magenta", "violet", "blue", "orange", "red", "gold", "silver", "bronze", "brown", "maroon", "cyan", "pink"
+              , "green", "turquoise", "indigo"]
+
+# B. Print the entire list
+print(color_list)
+
+# C. Access and print the 10th index
+print(color_list[10])
+
+# D. Update the 4th index to "Purple"
+color_list[4] = "purple"
+
+# E. Delete the 5th index
+del color_list[5]
+
+# F. Slice the list from index 2 to 8
+color_list[2:8]
+
+# G. Print the last index
+print(color_list[-1])
