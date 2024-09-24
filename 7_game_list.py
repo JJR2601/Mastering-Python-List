4eb44c828c4e39e7b379d756ab59257d23e034cf
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 17:16:00 +0800

    Manipulating the list of games

---
+++ 7_game_list.py
@@ -0,0 +1,21 @@
+# A. Create a list of 15 video game titles
+game_list = ["super mario", "dota", "league of legends", "assassin's creed", "fallout 2", "fortnite", "dark souls", "donkey kong",
+             "control", "call of duty", "apex legends", "counter strike", "left 4 dead", "read dead redemption", "tetris"]
+
+# B. Print the entire list
+print(game_list)
+
+# C. Access and print the 7th index
+print(game_list[7])
+
+# D. Update the 4th index to "Minecraft"
+game_list[4] = "minecraft"
+
+# E. Delete the 9th index
+del game_list[-6]
+
+# F. Slice the list from index 5 to 10
+game_list[5:10]
+
+# G. Print the last index
+print(game_list[-1])
