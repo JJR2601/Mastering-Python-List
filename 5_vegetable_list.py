fe915f38d48a7700ca6b6dc1ea97e5f905dbb01d
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 16:28:44 +0800

    Vegetable List Manipulation

---
+++ 5_vegetable_list.py
@@ -0,0 +1,19 @@
+# A. Create a list of 7 vegetables
+from operator import index
+
+vegetable_list = ["okra", "beeet", "radish", "cabbage", "broccoli", "squash", "carrot"]
+
+# B. Print the entire list
+print(vegetable_list)
+
+# C. Access and print the 5th index
+print(vegatable_list[5])
+
+# D. Update the 3rd index to "Spinach"
+vegetable_list[3] = "spinach"
+
+# E. Delete the 6th index
+del vegetable_list[6]
+
+# F. Print the last index
+print(vegetable_list[-1])
