9f6dfd2e95dc4f4d02f4c722c1bf4c91134428b1
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 19:40:21 +0800

    Assessing holiday list

---
+++ 19_holiday_list.py
@@ -0,0 +1,21 @@
+# A. Create a list of 12 holidays
+holiday = ["all saints day", "all souls day", "valentines day", "labor day", "independence day", "new years eve", "day of valor",
+           "holy week", "chinese new year", "rizal day", "thanks giving", "ninoy aquino day"]
+
+# B. Print the entire list
+print(holiday)
+
+# C. Access and print the 9th index
+print(holiday[-3])
+
+# D. Update the 3rd index to "Christmas"
+holiday[3] = "christmas"
+
+# E. Delete the 11th index
+del holiday[-1]
+
+# F. Slice the list from index 2 to 7
+holiday[2:7]
+
+# G. Print the last index
+print(holiday[-1])
