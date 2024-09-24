89c0cf1ee84bb1e5a9662614c4bc4856a0c4bd8f
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:56:12 +0800

    List of carpentry tools and accessing it

---
+++ 30_tool_list.py
@@ -0,0 +1,11 @@
+tools = ["pencil", "hammer", "screwdriver", "mallet", "chisel", "hand saw", "power drill", "sander", "nailer", "tape measure"]
+
+print(tools)
+
+print(tools[4])
+
+tools[-3] = "hammer"
+
+del tools[5]
+
+print(tools[-1])
