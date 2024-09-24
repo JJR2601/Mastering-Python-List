75604e61b9a030be95ca429e13388415023ebccd
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 21:36:40 +0800

    Well-known currencies around the world list

---
+++ 36_currency_list.py
@@ -0,0 +1,12 @@
+currency = ["dirham", "afghani", "argentine peso", "philippine peso", "australian dollar", "us dollar". "bahrain dinar",
+            "brazilian real", "euro", "rupee"]
+
+print(currency)
+
+print(currency[4])
+
+currency[-3] = "euro"
+
+del currency[-1]
+
+print(currency[-1])
