2a83001de9c56b0df7567335a677001c6a807e93
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 17:51:57 +0800

    Manipulating the list of beautiful flowers

---
+++ 10_flower_list.py
@@ -0,0 +1,17 @@
+# A. Create a list of 10 flower names
+flowers = ["allysum", "jasmine", "orchid", "sunflower", "tulip", "lavender", "hibiscus", "iris", "daisy", "ageratum"]
+
+# B. Print the entire list
+print(flowers)
+
+# C. Access and print the 5th index
+print(flowers[5])
+
+# D. Update the 8th index to "Rose"
+flowers[8] = "rose"
+
+# E. Delete the 2nd index
+del flowers[2]
+
+# F. Print the last index
+print(flowers[-1])
