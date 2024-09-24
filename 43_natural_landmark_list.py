d9a55fc5e6c06e58266433b284f35a18e865c870
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 22:28:42 +0800

    Manipulating List of 10 beatiful natural landmarks that i found

---
+++ 43_natural_landmark_list.py
@@ -0,0 +1,12 @@
+natural_landmark = ["the fly geyser", "crane cristales", "pulpit rock", "paria canyon", "jeita grotto", "lake nakuru"
+                    , "siwa oasis", "perito moreno glacier", "moraine lake", "jeju island"]
+
+print(natural_landmark)
+
+print(natural_landmark[-2])
+
+natural_landmark[5] = "grand canyon"
+
+del natural_landmark[-1]
+
+print(natural_landmark[-1])
