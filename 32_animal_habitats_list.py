f0fb2b9b4e0ae192e915c0d0684ffc0b58981cb5
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 21:18:07 +0800

    Manipulating list of animal habitats

---
+++ 32_animal_habitats_list.py
@@ -0,0 +1,11 @@
+habitats = ["tundra", "savanna", "tropical rainforest", "desert", "arctic", "coral reef", "mountains", "ocean"]
+
+print(habitats)
+
+print(habitats[5])
+
+habitats[3] = "savannah"
+
+del habitats[-2]
+
+print(habitats[-1])
