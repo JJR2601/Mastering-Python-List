9666b9a6a7c44317a6a9d8515cbece088a3ed1e1
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 23:00:21 +0800

    Manipulating the list of the 7 wonders of the world

---
+++ 49_world_wonders_list.py
@@ -0,0 +1,12 @@
+world_wonders = ["chichen itza", "petra" ,"machu picchu", "great wall of china", "christ the redeemer", "colosseum", "taj mahal"]
+
+print(world_wonders)
+
+print(world_wonders[4])
+
+world_wonders[2] = "great wall of china"
+
+del world_wonders[-2]
+
+print world_wonders[-1]
+
