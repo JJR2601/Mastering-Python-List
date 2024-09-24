dd9756c9ab461d99a3199b8357388e7d227f7327
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 19:06:59 +0800

    List of Pokemons

---
+++ 15_pokemon_list.py
@@ -0,0 +1,21 @@
+# A. Create a list of 15 Pokemon names
+pokemons = ["charmander", "bulbasaur", "caterpie", "weedle", "pidgey", "nidoran", "jigglypuff", "clefairy", "zubat", " oddish"
+            , "venonat", "girafarig", "wobbuffet", "pineco", "surskit"]
+
+# B. Print the entire list
+print(pokemons)
+
+# C. Access and print the 9th index
+print(pokemons[9])
+
+# D. Update the 12th index to "Pikachu"
+pokemons[12] = "pikachu"
+
+# E. Delete the 10th index
+del pokemons[10]
+
+# F. Slice the list from index 4 to 7
+pokemons[4:7]
+
+# G. Print the last index
+print(pokemons[-1])
