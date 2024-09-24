e302b7bf04b318f838ed4591c3a88f05ff5164b4
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 15:46:01 +0800

    Animal list Manipulation

---
+++ 1_animal_list.py
@@ -0,0 +1,17 @@
+# A. Create a list of 10 animal names
+animal_list = ["rhino", "cobra", "leopard", "falcon", "hippo", "raven", "whale", "spider", "squirrel", "horse"]
+
+# B. Print the entire list
+print(animal_list)
+
+# C. Access and print the 3rd index
+print(animal_list[3])
+
+# D. Update the 6th index to "Elephant"
+animal_list[6] = "elephant"
+
+# E. Delete the 8th index
+del animal_list[8]
+
+# Print the last index
+print(animal_list[-1])
