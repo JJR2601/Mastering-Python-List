f1073e744e36d2bd1928edf2dfc810b9b86068e9
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 21:56:07 +0800

    Manipulating the list of popular universities in the philippines

---
+++ 39_university_list.py
@@ -0,0 +1,12 @@
+university = ["diliman", "ateneo de manila", "de la salle", "santo tomas", "national university", "our lady of fatima"
+              , "san juan de letran", "ama", "feu", "new era"]
+
+print(university)
+
+print(university[6])
+
+university[4] = "harvard"
+
+del university[-1]
+
+print(university[-1])
