b6c992a7372a1cf4a15e0a3757a5298a4579a849
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 20:31:50 +0800

    Beautiful buildings in the world list manipulation

---
+++ 27_building_list.py
@@ -0,0 +1,12 @@
+buildings = ["pentagon", "eiffel tower", "taj mahal", "great wall of china", "machi picchu", "milwaukee art museum",
+             "st paul cathedral", "the white house"]
+
+print(buildings)
+
+print(buildings[5])
+
+buildings[2] = "eiffel tower"
+
+del buildings[-2]
+
+print(buildings[-1])
