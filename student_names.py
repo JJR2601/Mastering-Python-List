547229977fba5ba145f152dd190b371e05b0f193
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 15:42:16 +0800

    Animal List Manipulation

--- student_names.py
+++
@@ -1,4 +0,0 @@
-student_name_list = {"justine", "jerald", "jose", "julia", "jorge", "justin", "lilith", "ivy", "joshua", "jasmine",
-                    "aga", "jackson", "liz", "charlie", "noah", "tyler", "hiro", "olivia", "queen", "zosia"}
-
-print(student_name_list)
