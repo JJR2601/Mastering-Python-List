d83ab6d4b87116393807de74209b554fb4c7dcad
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 16:45:57 +0800

    Controlling the Movie lists

---
+++ 6_movie_list.py
@@ -0,0 +1,22 @@
+# A. Create a list of 20 movie titles
+movie_list = ["interstellar", "the matrix", "star wars", "terminator", "spirited away", "the pianist", "parasite", "the lion king"
+    , "memento", "avengers: infinity war", "avengers: endgame", "toy story", "das boot", "joker", "your name", "weathering away"
+              , "one piece film: red", "one piece: stampede", "3d2y", "up"]
+
+# B. Print the entire list
+print(movie_list)
+
+# C. Access and print the 12th index
+print(movie_list[12])
+
+# D. Update the 15th index to "Inception"
+movie_list[15] = "inception"
+
+# E. Delete the 18th index
+del movie_list[-2]
+
+# F. Slice the list from index 8 to 13
+movie_list[8:13]
+
+# G. Print the last index
+print(movie_list[-1])
