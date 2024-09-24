a753a40e31b1c7791a0598c52e0dc217f1ac7373
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 19:22:51 +0800

    Assessing list of famous figures in history

---
+++ 17_historical_figures_list.py
@@ -0,0 +1,18 @@
+# A. Create a list of 10 historical figures
+historical_figures = ["queen diana", "alexander the great", "julius caesar", "napoleon", "augustus", "genghis khan", "cleopatra"
+                      , "nero", "ramesses", "constantine"]
+
+# B. Print the entire list
+print(historical_figures)
+
+# C. Access and print the 8th index
+print(historical_figures[-2])
+
+# D. Update the 4th index to "Nelson Mandela"
+historical_figures[4] = "nelson mandela"
+
+# E. Delete the 7th index
+del historical_figures[-3]
+
+# F. Print the last index
+print(historical_figures[-1])
