53bdc9451ae98fda60694cd834afedbcbbcdb870
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-24 16:22:17 +0800

    Manipulating the fruit list

---
+++ 4_fruit_list.py
@@ -0,0 +1,21 @@
+# A. Create a list of 12 fruits
+fruit_list = ["orange", "dragonfruit", "pineapple", "apple", "banana", "pear", "grapes", "raspberry", "strawberry"
+              , "dalandan", "watermelon", "blueberry"]
+
+# B. Print the entire list
+print(fruit_list)
+
+# C. Access and print the 9th index
+print(fruit_list[9])
+
+# D. Update the 2nd index to "Mango"
+fruit_list[2] = "mango"
+
+# E. Delete the 10th index
+del fruit_list[10]
+
+# F. Slice the list from index 4 to 7
+fruit_list[4:7]
+
+# G. Print the last index
+print(fruit_list[-1])
